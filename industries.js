bf05ae3da31f74ae204c3d95116270cb44a2c703
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2022-05-25 13:25:54 -0400

    Optimizing work page

--- industries.js
+++ industries.js
@@ -1,86 +1,86 @@
 module.exports = [
   {
     text: 'Materials Handling',
-    img: '/images/industries/materialsHandling-min.webp'
+    img: '/images/industries/materialsHandling-min.jpg'
   },
   {
     text: 'Power Generation',
-    img: '/images/industries/powerGeneration-min.webp'
+    img: '/images/industries/powerGeneration-min.jpg'
   },
   {
     text: 'Environmental Services',
-    img: '/images/industries/environmentalServices-min.webp'
+    img: '/images/industries/environmentalServices-min.jpg'
   },
   {
     text: 'Chemicals & Refining',
-    img: '/images/industries/refining-min.webp'
+    img: '/images/industries/refining-min.jpg'
   },
   {
     text: 'Logistics',
-    img: '/images/industries/logistics-min.webp'
+    img: '/images/industries/logistics-min.jpg'
   },
   {
     text: 'Safety',
-    img: '/images/industries/safety-min.webp'
+    img: '/images/industries/safety-min.jpg'
   },
   {
     text: 'Manufacturing',
-    img: '/images/industries/manufacturing-min.webp'
+    img: '/images/industries/manufacturing-min.jpg'
   },
   {
     text: 'Industrials',
-    img: '/images/industries/industrials-min.webp'
+    img: '/images/industries/industrials-min.jpg'
   },
   {
     text: 'Civil Engineering',
-    img: '/images/industries/civilEngineering-min.webp'
+    img: '/images/industries/civilEngineering-min.jpg'
   },
   {
     text: 'Engineering Construction',
-    img: '/images/industries/construction-min.webp'
+    img: '/images/industries/construction-min.jpg'
   },
   {
     text: 'Mining',
-    img: '/images/industries/mining-min.webp'
+    img: '/images/industries/mining-min.jpg'
   },
   {
     text: 'Telecom',
-    img: '/images/industries/telecom-min.webp'
+    img: '/images/industries/telecom-min.jpg'
   },
   {
     text: 'Oil & Gas',
-    img: '/images/industries/oil-min.webp'
+    img: '/images/industries/oil-min.jpg'
   },
   {
     text: 'Marine',
-    img: '/images/industries/marine-min.webp'
+    img: '/images/industries/marine-min.jpg'
   },
   {
     text: 'Industrial Services',
-    img: '/images/industries/industrialServices-min.webp'
+    img: '/images/industries/industrialServices-min.jpg'
   },
   {
     text: 'Pneumatic Conveying',
-    img: '/images/industries/pneumaticConveying-min.webp'
+    img: '/images/industries/pneumaticConveying-min.jpg'
   },
   {
     text: 'Construction Trades',
-    img: '/images/industries/constructionTrades-min.webp'
+    img: '/images/industries/constructionTrades-min.jpg'
   },
   {
     text: 'Carpentry',
-    img: '/images/industries/carpentry-min.webp'
+    img: '/images/industries/carpentry-min.jpg'
   },
   {
     text: 'Electrical',
-    img: '/images/industries/electrical-min.webp'
+    img: '/images/industries/electrical-min.jpg'
   },
   {
     text: 'Glazing',
-    img: '/images/industries/glazing-min.webp'
+    img: '/images/industries/glazing-min.jpg'
   },
   {
     text: 'Equipment',
-    img: '/images/industries/equipment-min.webp'
+    img: '/images/industries/equipment-min.jpg'
   },
 ]
