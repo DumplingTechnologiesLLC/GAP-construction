07832c833bdb7057c0615046739392e1b48e2e41
Pedro Del Moral Lopez <pedro.del_moral_lopez.1@us.af.mil>
2022-06-20 13:42:43 -0400

    Add cache-clobbering version control to ensure updates go live correctly

--- site.config.js
+++ site.config.js
@@ -3,12 +3,15 @@ const works = require('./works');
 const awards = require('./awards');
 const employees = require('./employees');
 
+const { version } = require('./package.json');
+
 module.exports = {
   build: {
     srcPath: './src',
     outputPath: './public'
   },
   site: {
+    version,
     title: 'GAP Advisors',
     industries,
     works,
